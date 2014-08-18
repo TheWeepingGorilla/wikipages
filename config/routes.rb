f0ec1df8d08793c6f21f9b4c8f1ec0bf5dcc4943
Sean Morrison & Austin Steward <student@epicodus.com>
2014-08-18 11:56:26 -0700

    Init commit. Routs / Contoller / View setup.

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  Wikipages::Application.routes.draw do
+    match('contacts', {:via => :get, :to => 'contacts#index'})
+    match('contacts', {:via => :post, :to => 'contacts#create'})
+    match('contacts/new', {:via => :get, :to => 'contacts#new'})
+    match('contacts/:id', {:via => :get, :to => 'contacts#show'})
+  end
+end
+
+
